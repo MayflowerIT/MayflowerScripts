738acb605491260b7400950ef4011e49fdc8fb31
John D Pell <John@MayflowerIT.com>
2020-12-02 18:47:45 -0800

    reorg

--- Install-LogMeIn4Mayflower.ps1
+++ Install-LogMeIn4Mayflower.ps1
@@ -1,24 +1,20 @@
-﻿#lmi
+﻿#Requires -RunAsAdministrator
 
 if (-not (test-path env:LMIDEPLOYID)) 
 { 
     $env:LMIDEPLOYID = '01_7xa4jho24dqv5v4bfjhwasvsds7nyyrfb7fyb'
 }
 
-#tsji  DEPLOYID=01_gz162dh20kht8x39r8v2qawrm0ulqy248olpw
+$lmimsiurl = "HTTPS://SECURE.LOGMEIN.COM/LOGMEIN.MSI"
+$lmimsifile = "$ENV:SystemRoot\System\$(Split-Path -Path $lmimsiurl -Leaf)"
 
-$lmimsiurl = "https://secure.logmein.com/logmein.msi"
-$lmimsifile = "$env:temp\LogMeIn.msi"
+$command = "curl -Uri $lmimsiurl -OutFile $lmimsifile"
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+$encodedCommand = [Convert]::ToBase64String($bytes)
 
-Start-Service BITS
-Import-Module BitsTransfer
+$w32c = Get-WmiObject -Class Win32_OperatingSystem
+$lmidesc = $w32c.description
 
-Start-BitsTransfer -Source $lmimsiurl -Destination $lmimsifile -TransferType Download
-
-
-#TODO read the description from the registry to pre-populate... dunno how to do going forward since no sync...
-
-
-msiexec.exe /i "$lmimsifile" /qn "DEPLOYID=$env:LMIDEPLOYID" INSTALLMETHOD=5 FQDNDESC=1
-
+Invoke-WebRequest -Uri $lmimsiurl -OutFile $lmimsifile -ErrorAction STOP
+msiexec /i $lmimsifile /qn "DEPLOYID=$env:LMIDEPLOYID" INSTALLMETHOD=5 FQDNDESC=0 LMIDESCRIPTION="$ENV:ComputerName: $lmidesc"
 
