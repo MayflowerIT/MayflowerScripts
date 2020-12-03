0f20daf19a878aa2b27b7045c9309411d424d154
John D Pell <John@MayflowerIT.com>
2020-12-02 18:54:22 -0800

    Add MayflowerScripts module files

---
+++ Install-MSO365.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -RunAsAdministrator
+
+#TODO: -minimumVersion allows for expected update behavior without having to -Force, according to documentation
+
+Install-Module -Scope AllUsers -AllowClobber -Confirm:$false `
+        -Name   MSOnline,
+                AzureAD,
+                ExchangeOnlineManagement,
+                Microsoft.Online.SharePoint.PowerShell,
+                MicrosoftTeams
+                #-Force
+#                SharePointPnPPowerShellOnline,
+
+#Microsoft.Graph.Authentication (= 1.1.0)
+#Microsoft.Graph.Groups.Planner (= 0.9.1)
+#Microsoft.Graph.Identity.ConditionalAccess (= 0.9.1)
+#Microsoft.Graph.Intune (= 6.1907.1)
+#Microsoft.Graph.Planner (= 1.1.0)
+#Microsoft.PowerApps.Administration.PowerShell (= 2.0.99)
+#MsCloudLoginAssistant (= 1.0.42)
+
---
+++ Update-PowerShellPackageManagement.ps1
@@ -0,0 +1,12 @@
+﻿#Requires -RunAsAdministrator
+#Requires -Version 5.0
+#Requires -Modules PackageManagement
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Register-PSRepository -Default -InstallationPolicy Trusted -ErrorAction SilentlyContinue # Only needed if something gets corrupted...
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction STOP
+
+Install-Module -Name PackageManagement -Force -AllowClobber -Confirm:$false # This should be auto-installed by the next line anyway
+Install-Module -Name PowerShellGet -Force -AllowClobber -Confirm:$false
