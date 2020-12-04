0c8280a312c073e55e9825b2af832d955ce03858
John D Pell <John@MayflowerIT.com>
2020-12-03 19:05:30 -0800

    Add LAPS configuration script

---
+++ Configure-LAPSSchemaAndPermissions.ps1
@@ -0,0 +1,15 @@
+﻿#Requires -RunAsAdministrator
+#Requires -Module AdmPwd.PS
+Import-Module AdmPwd.PS
+
+$cs = Get-ComputerInfo
+$ADDS = $cs.CsDomain
+$ADDSDN = $($ADDS.split(".") | % { "DC=$_".trim() }) -join ","
+
+$ADDSforest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()           
+$ADDSdomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+
+
+Update-AdmPwdADSchema
+Set-AdmPwdComputerSelfPermission -Identity $ADDSDN
