35c5e314dfec9526a3f7d456105a78f892da3688
John D Pell <John@MayflowerIT.com>
2020-12-22 15:47:18 -0800

    Reduce use of -Force parameter

--- Update-PowerShellPackageManagement.ps1
+++ Update-PowerShellPackageManagement.ps1
@@ -4,9 +4,14 @@
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope AllUsers 
+#-Force
 Register-PSRepository -Default -InstallationPolicy Trusted -ErrorAction SilentlyContinue # Only needed if something gets corrupted...
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction STOP
 
-Install-Module -Name PackageManagement -Force -AllowClobber -Confirm:$false # This should be auto-installed by the next line anyway
-Install-Module -Name PowerShellGet -Force -AllowClobber -Confirm:$false
+#Install-Module -Name PackageManagement -AllowClobber -Confirm:$false # This should be auto-installed by the next line anyway
+Install-Module -Name PowerShellGet -AllowClobber -Scope AllUsers -MinimumVersion 2.0 -WarningAction STOP
+if ($False -eq $?)
+{
+	Install-Module -Name PowerShellGet -AllowClobber -Scope AllUsers -MinimumVersion 2.0 -WarningAction STOP -FORCE -verbose
+}
