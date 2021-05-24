b040d3baee2df24ccfc2a79516f3dff3165c7b61
John D Pell <John@MayflowerIT.com>
2021-05-24 16:00:28 -0400

    VMwareTools: configuration resource

--- DSCresources/VMwareTools/VMwareTools.schema.psm1
+++ DSCresources/VMwareTools/VMwareTools.schema.psm1
@@ -2,12 +2,16 @@
 <# scratch for DSC ideas #>
 
 New-Variable -Option ReadOnly -Name SystemSixteen -Value (Join-Path $env:SystemRoot "System")
+$VMwT = Join-Path $SystemSixteen "VMware-tools.exe"
 
 Configuration VMwareTools
-{
-	Param(
-		Path = Join-Path $SystemSixteen "VMware-tools.exe",
-		Uri = [Uri]"https://packages.vmware.com/tools/esx/latest/windows/x64/VMware-tools-11.2.5-17337674-x86_64.exe"
+{	Param(
+		[String]
+		$Path = $VMwT,
+		[Uri]
+		$Uri = "https://packages.vmware.com/tools/esx/latest/windows/x64/VMware-tools-11.2.5-17337674-x86_64.exe",
+		[Guid]
+		$ProductID = ""
 	)
 
 	Import-DscResource xPSDesiredStateConfiguration
