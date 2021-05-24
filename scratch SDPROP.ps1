ae746547e2edd3f3eebe0d5bc6c332d0d74545e8
John D Pell <John@MayflowerIT.com>
2021-05-24 16:01:26 -0400

    Update scratch SDPROP.ps1

--- scratch SDPROP.ps1
+++ scratch SDPROP.ps1
@@ -6,7 +6,8 @@
 $asdh = get-acl 'AD:\cn=AdminSDHolder,cn=System,DC=Mayflower,DC=IT'
 
 # All accounts which have AdminCount set to zero-or-greater (i.e., not blank), which are not AD-internals
-$ACs = Get-ADUser -Filter {AdminCount -ge '0' -and IsCriticalSystemObject -notlike '*'} -Properties NTSecurityDescriptor
+$ACs = Get-ADUser -Filter {AdminCount -ge '0' -and IsCriticalSystemObject -notlike '*'} -Properties NTSecurityDescriptor -ResultSetSize $null
+#([adsisearcher]"(AdminCount=1)").findall()
 
 # accounts with settings as configured, but not neccesarily actually admins...
 $MaybeAdmins = $ACs | where {$_.NTSecurityDescriptor.AccessToString -eq $asdh.AccessToString}
@@ -26,6 +27,7 @@ $FormerAdmins | TODO#: enable inheritance, then clear explicit ACEs
 if($Force)
 {
 	Get-ADUser -Filter {AdminCount -ge '0' -and IsCriticalSystemObject -notlike '*'} | Set-ADUser -Replace @{adminCount=0} -passthru | set-acl $whatever
+
 }
 # https://github.com/edemilliere/ADSI/blob/master/Invoke-ADSDPropagation.ps1
 Invoke-ADSDPropagation -TaskName RunProtectAdminGroupsTask
@@ -35,3 +37,10 @@ Invoke-ADSDPropagation -TaskName RunProtectAdminGroupsTask
 # https://specopssoft.com/blog/troubleshooting-user-account-permissions-adminsdholder/
 # returns *groups* which are protected by SDPROP
 foreach ($group in $MaybeAdmins.memberof) { get-adgroup $group -properties adminCount | where {$_.adminCount -ge 0} }
+#TODO: default protected groups are all critical system objects, so look for groups which are not ultimately members of any of them which in turn indicates they should have AdminCount decremented and ACL cleared.
+#$user.ntsecuritydescriptor.SetAccessRuleProtection($false,$true)
+#set-aduser <username> -replace @{ntsecuritydescriptor=$user.ntsecuritydescriptor}
+
+
+
+#TODO: remove Account Holders from SDPROP via dsHeuristics
